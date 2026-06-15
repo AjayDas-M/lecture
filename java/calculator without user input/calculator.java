56ea9c332763c1185da787d2c00bcb7f6c1a2d68
Ajay Das <147685811+AjayDas-M@users.noreply.github.com>
2026-06-15 09:51:20 +0530

    bacics java

---
+++ java/calculator without user input/calculator.java
@@ -0,0 +1,22 @@
+//calculator without user input
+class sum
+{
+	public static void main(String[] args)
+	{
+		// values to be calculated
+		int a = 22;
+		double b = 12;
+		
+		//SUM of a and be
+		System.out.println("The SUM of " + a + " and " + b + " is " + (a+b));
+		
+		//The Differennce of a and b is
+		System.out.println("The difference of " + a + " and " + b + " is " + (a-b));
+		
+		//The Product of a and b is 
+		System.out.println("The Product of " + a +" and " + b + " is " + (a*b));
+		
+		//a divided by b is  
+		System.out.println("The division of " + a +" and " + b + " is " + (a/b));
+	}
+}
