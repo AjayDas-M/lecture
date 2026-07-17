d6733eba7b95241f2e96c0d811848a857bc106b7
Ajay Das <147685811+AjayDas-M@users.noreply.github.com>
2026-07-17 12:43:48 +0530

    ignore

---
+++ c/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter first number : ");
+	scanf("%d",&a);
+	printf("Enter second  number : ");
+	scanf("%d",&b);
+	printf("The sum od %d and %d is %d",a,b,a+b);
+	return 0 ;
+
+}
