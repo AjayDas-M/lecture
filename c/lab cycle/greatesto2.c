851691e754a1464b644bff559682d4ebd312f80a
Ajay Das <147685811+AjayDas-M@users.noreply.github.com>
2026-07-24 13:31:06 +0530

    changes

--- c/lab cycle/greatesto2.c
+++ c/lab cycle/greatesto2.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-	int a = 30,b = 20,great;
-	//int great = (a>b)?a:b;
+	int a = 10,b = 20,great;
+	(a>b)?printf("%d is the greatest",a):printf("%d is the greatest",b);
 	(a>b)?(great = a): (great = b);
 	printf("%d is great",great);
 	return 0;
